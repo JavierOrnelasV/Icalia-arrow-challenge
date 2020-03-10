00b708c54350d56d3ce2f913cabde29fc75d50ff
Javier <naitomea351@gmail.com>
2020-03-10 15:47:31 -0600

    add navbar and landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root to: "landing#index"
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
