c07bcdd30574d2f12943bf675a58416d9fa4b988
Javier <naitomea351@gmail.com>
2020-03-13 13:18:13 -0600

    added changes to associations

--- app/controllers/arrows_controller.rb
+++ app/controllers/arrows_controller.rb
@@ -1,36 +1,19 @@
 class ArrowsController < ApplicationController
   def index
-<<<<<<< HEAD
     @arrows = current_user.received_arrows
   end
 
   def new
     @arrow = current_user.created_arrows.new
-=======
-    @arrows = Arrow.where(destination: current_user)
-  end
-
-  def new
-    @arrow = current_user.arrows.new
->>>>>>> 44524c6c7d2f878cb9667d1547a5fca000b40e68
     @users = User.all
   end
 
   def create
     destination = User.find(params[:arrow][:destination])
     reason = params[:arrow][:reason]
-<<<<<<< HEAD
     current_user.created_arrows.create(destination: destination, reason: reason)
     redirect_to arrows_url
   end
 
   def show; end
-=======
-    current_user.arrows.create(destination: destination, reason: reason)
-    redirect_to arrows_url
-  end
-
-  def show
-  end
->>>>>>> 44524c6c7d2f878cb9667d1547a5fca000b40e68
 end
--- spec/models/arrow_spec.rb
+++ spec/models/arrow_spec.rb
@@ -13,17 +13,10 @@ RSpec.describe Arrow, type: :model do
                                   password: '123456',
                                   password_confirmation: '123456' )
 
-<<<<<<< HEAD
       arrow = creator.created_arrows.create!( destination: destination, reason: 'I wanted to create an arrow' )
 
       saved_arrow = destination.received_arrows.last
 
-
-=======
-      arrow = creator.arrows.create!( destination_id: destination.id, reason: 'I wanted to create an arrow' )
-
-      saved_arrow = Arrow.find_by(reason: 'I wanted to create an arrow')
->>>>>>> 44524c6c7d2f878cb9667d1547a5fca000b40e68
 
       expect(arrow).to eql(saved_arrow)
     end
